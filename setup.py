bb8bde843abf17831d98621b8bdb5977226b6e5a
Lei CT Zhu <lei.ctzhu@gmail.com>
2017-10-31 09:26:38 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,5 @@ setup(name='MIPCL',
       version='1.41',
       packages=['mipcl_py'],
       description='Mix Integer Programming Class Library',
-      classifiers=['Programming Language :: Python :: 3.5 :: Only'],
       license='GLPL v3'
       )
